351e18fe212b575a2584416c9d7b2e3073786943
Alex Dean <github@mostlyalex.com>
2018-09-05 11:52:46 -0500

    actioncable & heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+production_domain = 'focus-group.herokuapp.com'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -40,8 +42,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://#{production_domain}/cable"
+  config.action_cable.allowed_request_origins = [ "https://#{production_domain}/" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
