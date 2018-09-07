e5a04b0b633e9ba78b92851cac77dbf937797d2f
Zebulon Young <zebulonj@gmail.com>
2018-09-07 10:40:25 -0700

    Add dragging to slider.

--- app/javascript/components/ScoreInput/ScoreInput.js
+++ app/javascript/components/ScoreInput/ScoreInput.js
@@ -7,17 +7,21 @@ export class ScoreInput extends Component {
     super( props );
 
     this.state = {
+      engaged: false,
       score: 0
     };
 
     this.onClick = this.onClick.bind( this );
+    this.onMouseDown = this.onMouseDown.bind( this );
+    this.onMouseUp = this.onMouseUp.bind( this );
+    this.onMouseMove = this.onMouseMove.bind( this );
   }
 
   render() {
     const { labels: { min, max } = {} } = this.props;
 
     return (
-      <div className={ styles.wrapper } onClick={ this.onClick }>
+      <div className={ styles.wrapper } onClick={ this.onClick } onMouseDown={ this.onMouseDown }>
         <div className={ styles.min }>{ min }</div>
         <div className={ styles.max }>{ max }</div>
         <div className={ styles.track }></div>
@@ -34,8 +38,32 @@ export class ScoreInput extends Component {
     onChange( ( evt.clientX - left ) / width );
   }
 
+  onMouseDown( evt ) {
+    this.el = evt.target;
+    this.setState({ engaged: true });
+  }
+
+  onMouseUp() {
+    this.setState({ engaged: false });
+  }
+
+  onMouseMove( evt ) {
+    if ( !this.state.engaged ) return;
+
+    const { onChange } = this.props;
+
+    const { left, width } = this.el.getBoundingClientRect();
+    const score = ( evt.clientX - left ) / width;
+
+    this.setState({ score });
+    onChange( score );
+  }
+
   componentDidMount() {
     const { onChange } = this.props;
+
+    window.addEventListener( 'mousemove', this.onMouseMove );
+    window.addEventListener( 'mouseup', this.onMouseUp );
 
     this.tick = setInterval( () => {
       onChange( this.state.score );
