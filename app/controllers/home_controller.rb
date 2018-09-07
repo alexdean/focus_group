12e5a1d612eb99c2e38a9513aa883c7c6bb044f4
Alex Dean <github@mostlyalex.com>
2018-09-07 11:24:15 -0500

    base-bones configuration for issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def admin
+    @issue = Issue.fetch
+
+    if ENV['ADMIN_KEY'] && params[:key] == ENV['ADMIN_KEY']
+      @issue.q = params[:q] if params[:q]
+      @issue.min = params[:min] if params[:min]
+      @issue.max = params[:max] if params[:max]
+      @issue.save
+    end
+  end
 end
