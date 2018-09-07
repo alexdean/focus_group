6a283e57854fa9c040fe2e12020ba6e8e2dea6e2
Zebulon Young <zebulonj@gmail.com>
2018-09-07 12:30:11 -0700

    Sort graph rows by name.

--- app/javascript/components/Graph/Graph.js
+++ app/javascript/components/Graph/Graph.js
@@ -5,14 +5,16 @@ import styles from './Graph.css';
 export const Graph = ({ data = [] }) => (
   <div className={ styles.wrapper }>
     {
-      data.map( function( item, idx ) {
-        return (
-          <div key={ item.uuid || idx } className={ styles.row }>
-            <div className={ styles.bar } style={{ width: `${ item.value * 100 }%` }}></div>
-            <div className={ styles.label }>{ item.name }</div>
-          </div>
-        );
-      })
+      data
+        .sort( ({ name: A }, { name: B }) => A.localeCompare( B ) )
+        .map( function( item, idx ) {
+          return (
+            <div key={ item.uuid || idx } className={ styles.row }>
+              <div className={ styles.bar } style={{ width: `${ item.value * 100 }%` }}></div>
+              <div className={ styles.label }>{ item.name }</div>
+            </div>
+          );
+        })
     }
   </div>
 );
